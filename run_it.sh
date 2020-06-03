eeb14197400c37dcf7330e243468fba5705ef226
mcgo0092 <mcgo0092@umn.edu>
2020-06-03 15:03:43 -0500

    remove fixed lamda at manual cd

--- run_it.sh
+++ run_it.sh
@@ -1,7 +1,7 @@
 docker stop cf_galaxy && docker rm cf_galaxy &&
-docker rmi tmcgowan/cloudforest_galaxy:0.5.1 &&
+docker rmi tmcgowan/cloudforest_galaxy:0.5.4 &&
 rm cloudforest.tar.gz &&
 cp ~/CodeProjects/JS/cloudforest_two/cloudforest.tar.gz . &&
-docker build -t tmcgowan/cloudforest_galaxy:0.5.1 . &&
-docker run -d -p 8080:80 --name cf_galaxy -e "NONUSE=nodejs,proftp,reports" -e "GALAXY_DESTINATIONS_DEFAULT=local_no_container" -e "GALAXY_SLOTS=4" tmcgowan/cloudforest_galaxy:0.5.1 &&
+docker build -t tmcgowan/cloudforest_galaxy:0.5.4 . &&
+docker run -d -p 8080:80 --name cf_galaxy -e "NONUSE=nodejs,proftp,reports" -e "GALAXY_DESTINATIONS_DEFAULT=local_no_container" -e "GALAXY_SLOTS=4" tmcgowan/cloudforest_galaxy:0.5.4 &&
 docker logs -f cf_galaxy
