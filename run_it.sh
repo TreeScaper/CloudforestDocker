1954defd1b17d58fbde84bf1d7046793910e43fd
Reid Wagner <wagnerr@umn.edu>
2021-02-22 19:10:20 -0600

    Modify run_it.sh: - Take three arguments: tag, container name, and optionally a volume to attach for data persistance. - Remove lines to copy in new visualization code.

--- run_it.sh
+++ run_it.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
-  then
-    echo "No container version supplied"
+if [[ $# -ne 2 && $# -ne 3 ]]; then
+    echo "Error: incorrect number of arguments."
+    echo "Usage: sh run_it.sh <tag> <container_name> [volume_name]"
+    echo
+    echo "The arguments provide the tag and name for the image and container that are created, respectively."
+    echo "Set them however you choose."
+    echo
+    echo "The third argument is to optionally provide the name of a pre-existing volume for data persistance."
     exit 1
 fi
 
-VERSION=$1
+TAG=$1
+CONTAINER_NAME=$2
 
-docker stop cf_galaxy && docker rm cf_galaxy &&
-docker rmi cloudforestphylogenomics/cloudforest_galaxy:$VERSION &&
-docker volume prune -f &&
-rm cloudforest.tar.gz &&
-cp ../CloudforestVisualization/cloudforest.tar.gz . &&
-docker build -t cloudforestphylogenomics/cloudforest_galaxy:$VERSION . &&
-docker run -d -p 8080:80 --name cf_galaxy -e "NONUSE=nodejs,proftp,reports" -e "GALAXY_CONFIG_CLEANUP_JOB=onsuccess" -e "GALAXY_DESTINATIONS_DEFAULT=local_no_container" -e "GALAXY_SLOTS=4" cloudforestphylogenomics/cloudforest_galaxy:$VERSION &&
-docker logs -f cf_galaxy
+VOLUME_ARGUMENT=""
+if [[ $# -eq 3 ]]; then
+   VOLUME_ARGUMENT="--mount source=${3},target=/export/"
+fi
+
+docker stop $CONTAINER_NAME && docker rm $CONTAINER_NAME
+docker rmi cloudforestphylogenomics/cloudforest_galaxy:$TAG
+
+docker build -t cloudforestphylogenomics/cloudforest_galaxy:$TAG . &&
+docker run -d -p 8080:80 $VOLUME_ARGUMENT --name $CONTAINER_NAME -e "NONUSE=nodejs,proftp,reports" -e "GALAXY_CONFIG_CLEANUP_JOB=onsuccess" -e "GALAXY_DESTINATIONS_DEFAULT=local_no_container" -e "GALAXY_SLOTS=4" cloudforestphylogenomics/cloudforest_galaxy:$TAG &&
+docker logs -f $CONTAINER_NAME
