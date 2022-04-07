be5a94db4787793215a531450f0a8c1557010ea0
Reid Wagner <wagnerr@umn.edu>
2022-04-07 14:37:04 -0500

    Visualization version number is now displayed on visualizatin interface. The bundle.js file is also now versioned in the filename, so that cached versions are not used by the browser.

--- copy_js.sh
+++ copy_js.sh
@@ -16,10 +16,10 @@ fi
 
 echo "$0 Copy files into container."
 docker cp cloudforest/templates/cloudforest.mako  cloudforest:/galaxy-central/static/plugins/visualizations/cloudforest/templates/
-docker cp cloudforest/static/bundle.js cloudforest:/galaxy-central/static/plugins/visualizations/cloudforest/static
+docker cp cloudforest/static/bundle*.js cloudforest:/galaxy-central/static/plugins/visualizations/cloudforest/static
 docker cp cloudforest/config/cloudforest.xml cloudforest:/galaxy-central/static/plugins/visualizations/cloudforest/config
 docker cp cloudforest/templates/cloudforest.mako  cloudforest:/galaxy-central/config/plugins/visualizations/cloudforest/templates/
-docker cp cloudforest/static/bundle.js cloudforest:/galaxy-central/config/plugins/visualizations/cloudforest/static
+docker cp cloudforest/static/bundle*.js cloudforest:/galaxy-central/config/plugins/visualizations/cloudforest/static
 docker cp cloudforest/config/cloudforest.xml cloudforest:/galaxy-central/config/plugins/visualizations/cloudforest/config
 
 if [[ $RESTART_GALAXY == true ]]; then
